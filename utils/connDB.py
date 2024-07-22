d894298b8b35124740658d608465d5a97916e15a
kanyesteves <kany.esteves@gmail.com>
2024-07-22 07:50:24 -0300

    Remover utils/connDB.py do índice do Git

--- utils/connDB.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-
-
-PATH_TO_DB = 'mysql+pymysql://root:#GIK64LK:;)IP//"@34.134.251.247/volatex'
-
-
-class ConnectDB():
-    def __init__(self):
-        self.engine = create_engine(PATH_TO_DB)
